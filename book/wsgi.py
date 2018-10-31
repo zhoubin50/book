80f3eefa306fb7b966bc9bf36069b60c8934ecb8
zhoubin50 <414330705@qq.com>
2018-10-31 10:41:23 +0800

    nodify wsgi

--- book/wsgi.py
+++ book/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+from os.path import join,dirname,abspath
 
 from django.core.wsgi import get_wsgi_application
 
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+import sys
+sys.path.insert(0,PROJECT_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "book.settings")
 
 application = get_wsgi_application()
